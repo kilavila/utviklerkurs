6b26589ef64eebe60abe03b1f046b2b92d385e6f
kilavila <christer.kilavik@protonmail.com>
2023-12-21 09:01:32 +0100

    Init

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { LandingPageComponent } from './landing-page/landing-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: LandingPageComponent,
+    pathMatch: 'full',
+  },
+  {
+    path: '**',
+    redirectTo: '',
+    pathMatch: 'full',
+  }
+];
---
+++ src/app/navbar/navbar.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar',
+  standalone: true,
+  imports: [],
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.scss'
+})
+export class NavbarComponent implements OnInit {
+
+  darkMode: boolean = false;
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.checkDarkMode();
+  }
+
+  checkDarkMode() {
+    const theme = localStorage.getItem('utviklerkurs-theme');
+    if (theme === 'dark') {
+      this.darkMode = true;
+    }
+  }
+
+  toggleDarkMode() {
+    this.darkMode = !this.darkMode;
+
+    localStorage.setItem('utviklerkurs-theme', this.darkMode ? 'dark' : 'light');
+  }
+
+}
