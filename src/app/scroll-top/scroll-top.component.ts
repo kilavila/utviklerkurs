222d7d0f4faa9ef8540ad02310c336b5d00db990
kilavila <christer.kilavik@protonmail.com>
2024-01-03 22:04:03 +0100

    Removed custom scrollbar

---
+++ src/app/scroll-top/scroll-top.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-scroll-top',
+  standalone: true,
+  imports: [],
+  templateUrl: './scroll-top.component.html',
+  styleUrl: './scroll-top.component.scss'
+})
+export class ScrollTopComponent {
+
+}
